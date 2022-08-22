5e287f69db998b0037d80603a8be99cc5bfeb538
Edvin Loberg <edvin.loberg@gmail.com>
2022-08-22 12:27:15 +0200

    add some variables

--- bin/tetris.dart
+++ bin/tetris.dart
@@ -1,3 +1,28 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:math';
+
+import 'package:console/console.dart';
+
+int gridSizeX = 20;
+int gridSizeY = 20;
+
+int score = 0;
+
+const controls = [
+  'up',
+  'down',
+  'left',
+  'right',
+  'w',
+  'a',
+  's',
+  'd',
+  'f',
+  ' ',
+  '', // escape
+];
+
 void main() {
   // nvm i dont feel like working on this rn
 }
